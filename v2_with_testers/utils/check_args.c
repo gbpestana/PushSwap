9e21ee955419aac224d2676ffc24215bbb4a2fd7
Gabriel Rodrigues Santos Pestana <grodrig2@c1r7p2.42sp.org.br>
2026-01-19 17:39:20 -0300

    add v2 and testers

---
+++ v2_with_testers/utils/check_args.c
@@ -0,0 +1,75 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   check_args.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: grodrig2 <grodrig2@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/19 09:45:20 by grodrig2          #+#    #+#             */
+/*   Updated: 2026/01/19 17:18:15 by grodrig2         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+static int	ft_contains(int num, char **argv, int i)
+{
+	i++;
+	while (argv[i])
+	{
+		if (ft_atoi(argv[i]) == num)
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+static int	ft_isnum(char *num)
+{
+	int	i;
+
+	i = 0;
+	if (num[0] == '-')
+		i++;
+	while (num[i])
+	{
+		if (!ft_isdigit(num[i]))
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+void	ft_check_args(int argc, char **argv)
+{
+	int			i;
+	long long	tmp;
+	char		**args;	
+
+	i = 0;
+	if (argc == 2)
+	{
+		args = ft_split(argv[1], ' ');
+		if (!args || !args[0])
+			ft_error("Error");	
+	}
+	else
+	{
+		i = 1;
+		args = argv;
+	}
+	while (args[i])
+	{
+		tmp = ft_atoi(args[i]);
+		if (!ft_isnum(args[i]))
+			ft_error("Error");
+		if (ft_contains(tmp, args, i))
+			ft_error("Error");
+		if (tmp < -2147483648 || tmp > 2147483647)
+			ft_error("Error");
+		i++;
+	}
+	if (argc == 2)
+		ft_free_split(args);
+}
+
---
+++ v2_with_testers/utils/lists.c
@@ -0,0 +1,78 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   lists.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: grodrig2 <grodrig2@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/19 09:45:05 by grodrig2          #+#    #+#             */
+/*   Updated: 2026/01/19 09:45:06 by grodrig2         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+t_stack	*ft_stacknew(int value)
+{
+	t_stack	*new;
+
+	new = (t_stack *) malloc(sizeof(*new));
+	if (!new)
+		return (NULL);
+	new->value = value;
+	new->index = -1;
+	new->next = NULL;
+	return (new);
+}
+
+void	ft_stackadd_front(t_stack **stack, t_stack *new)
+{
+	new->next = *stack;
+	*stack = new;
+}
+
+t_stack	*ft_stacklast(t_stack *head)
+{
+	t_stack	*tmp;
+
+	tmp = head;
+	while (tmp->next)
+	{
+		tmp = tmp->next;
+		if (tmp->next == NULL)
+			return (tmp);
+	}
+	return (tmp);
+}
+
+void	ft_stackadd_back(t_stack **stack, t_stack *new)
+{
+	t_stack	*n;
+
+	if (*stack)
+	{
+		n = ft_stacklast(*stack);
+		n->next = new;
+		new->next = NULL;
+	}
+	else
+	{
+		*stack = new;
+		(*stack)->next = NULL;
+	}
+}
+
+int	ft_stacksize(t_stack *head)
+{
+	size_t	i;
+	t_stack	*tmp;
+
+	tmp = head;
+	i = 0;
+	while (tmp)
+	{
+		tmp = tmp->next;
+		i++;
+	}
+	return (i);
+}
